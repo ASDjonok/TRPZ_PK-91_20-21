49c109fde5c541cf718f5c81f6dbd027b83ef300
Oleksii Aleshchenko <alexey.aleshchenko@gmail.com>
2020-09-01 12:05:07 +0300

    add lab1

--- src/MyClass.java
+++ src/MyClass.java
@@ -3,6 +3,20 @@ public class MyClass {
         System.out.println("Done!2");
         System.out.println(args[0]);
         System.out.println(new MyClass().myMethod());
+
+        char a = 'b';
+        char b = 'a';
+        System.out.println(a);
+        System.out.println((short)a);
+        System.out.println(b);
+        System.out.println((int)b);
+        System.out.println(a+b);
+
+        for (double i = 0.1; i < 0.2; i++) {
+            System.out.println("i: " + i);
+        }
+
+        System.out.println("ints division:" + ((double)1/2));
     }
 
     /*static*/ int myMethod() {
