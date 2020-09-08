24be9fc5184a62f990f670ca028631c1ba79e33a
Oleksii Aleshchenko <alexey.aleshchenko@gmail.com>
2020-09-08 11:44:10 +0300

    add interesting task

---
+++ src/interesting_task/Task.java
@@ -0,0 +1,10 @@
+package interesting_task;
+
+public class Task {
+    public static void main(String[] args) {
+        int n = 20;
+        for (int i = 0; i < n; i--) {
+            System.out.println("*");
+        }
+    }
+}
