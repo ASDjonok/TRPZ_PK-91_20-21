621847fc932044d9b62d6060344b3ae49997c3c4
Oleksii Aleshchenko <alexey.aleshchenko@gmail.com>
2020-09-15 09:31:36 +0300

    add content to lab2

--- src/Lab2.java
+++ src/Lab2.java
@@ -1,8 +1,20 @@
+import java.util.Arrays;
+
 public class Lab2 {
     public static void main(String[] args) {
 
 //        int[] array = new int[4];
         int[] array = {2, 3};
+        System.out.println(Arrays.toString(array));
+        System.out.print("[");
+        for (int i = 0; i < array.length - 1; i++) {
+            System.out.print(array[i] + ", ");
+        }
+        System.out.println(array[array.length-1] + "]");
+
+        for (int element : array) {
+            System.out.println(element);
+        }
 
 //        array[0] = 2;
         System.out.println(array[0]);
@@ -20,8 +32,14 @@ public class Lab2 {
         System.out.println("length = " + ma.length);
         for (int i = 0; i < ma.length; i++) {
             for (int j = 0; j < ma[i].length; j++) {
-
+                System.out.print(ma[i][j] + "\t");
             }
+            System.out.println();
+        }
+
+        final int columnNumber = 2;
+        for (int i = 0; i < ma.length; i++) {
+            System.out.println(ma[i][columnNumber]);
         }
 
         System.out.println("Done!");
