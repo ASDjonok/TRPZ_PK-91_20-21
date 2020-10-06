7e2e936b38a0907f801547009ff4ee4c90b86303
Oleksii Aleshchenko <alexey.aleshchenko@gmail.com>
2020-10-06 10:06:23 +0300

    add content of lab4

--- src/lab4/Cosmetics.java
+++ src/lab4/Cosmetics.java
@@ -1,6 +1,6 @@
 package lab4;
 
-public class Cosmetics {
+public class Cosmetics implements Comparable<Cosmetics> {
     private int cost;
     private String brand;
     private int volume;
@@ -15,6 +15,8 @@ public class Cosmetics {
         this.manufacturerCountry = manufacturerCountry;
     }
 
+
+
     @Override
     public String toString() {
         return "Cosmetics{" +
@@ -24,5 +26,38 @@ public class Cosmetics {
                 ", qualityLevel=" + qualityLevel +
                 ", manufacturerCountry='" + manufacturerCountry + '\'' +
                 "}\n";
+    }
+
+    public int getCost() {
+        return cost;
+    }
+
+    public void setCost(int cost) {
+        this.cost = cost;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    /*@Override
+    public int compareTo(Object o) {
+        return 0;
+    }*/
+
+    @Override
+    public int compareTo(Cosmetics o) {
+//        return /*this.*/cost - o.cost;
+        /*if (cost < o.cost) {
+            return -1;
+        } else {
+            if (cost > o.cost) {
+                return 1;
+            } else {
+                return 0;
+            }
+        }*/
+        int compareResult = Integer.compare(cost, o.cost);
+        return compareResult != 0 ? compareResult : o.brand.compareTo(brand);
     }
 }
--- src/lab4/Main.java
+++ src/lab4/Main.java
@@ -19,14 +19,22 @@ public class Main {
         Cosmetics[] cosmeticsArray = {
                 new Cosmetics(1, "L'Oreal Paris 1", 10, 10, "France"),
                 new Cosmetics(1, "L'Oreal Paris 5", 1, 10, "France"),
-                new Cosmetics(1, "L'Oreal Paris 3", 5, 10, "France"),
-                new Cosmetics(1, "L'Oreal Paris 123", 6, 10, "France"),
-                new Cosmetics(1, "L'Oreal Paris 5", 3, 10, "France"),
+                new Cosmetics(2, "L'Oreal Paris 3", 5, 10, "France"),
+                new Cosmetics(6, "L'Oreal Paris 123", 6, 10, "France"),
+                new Cosmetics(4, "L'Oreal Paris 5", 3, 10, "France"),
                 new Cosmetics(1, "L'Oreal Paris 4", 2, 10, "France"),
         };
 
         System.out.println("\u001B[35m" + Arrays.toString(cosmeticsArray));
 
+//        cosmeticsArray[0].setCost(2);
+        cosmeticsArray[0].setCost(Integer.MIN_VALUE);
+        System.out.println(cosmeticsArray[0].getCost());
+
+        Arrays.sort(cosmeticsArray);
+        System.out.println("\u001B[35m" + Arrays.toString(cosmeticsArray));
+
+//        System.out.println(cosmeticsArray[0].compareTo(cosmeticsArray[1]));
 //        cosmeticsArray[0].cost = 1;
 
 //        System.out.println(cosmetics);
